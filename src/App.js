86c1c7834195d85b2b8a195c6b46de94073cd120
genearellano <145499451+genearellano@users.noreply.github.com>
2023-09-30 13:40:08 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Demo } from './Demo';
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Demo></Demo>
+        <Navbar />
       </header>
     </div>
   );
