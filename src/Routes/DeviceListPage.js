dc769842eb19c24fd71aacf2f2ba105d53a3534d
Mohamed Burhan <maburhan711@gmail.com>
2023-11-12 20:50:36 -0800

    Added table for test list page and created communication betweeen interface and vendia client. ALso updated vendia folder for new schema after addiing device entitiy.

--- src/Routes/DeviceListPage.js
+++ src/Routes/DeviceListPage.js
@@ -1,37 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { vendiaClient } from '../vendiaClient';
+import { Table, Button, Modal } from 'react-bootstrap';
 
-const DummyData = [
-    {
-        sectionId: 1,
-        orgAssignment: 'Org 1',
-        name: 'John Doe',
-        method: 'Method A',
-        notes: 'Lorem ipsum dolor sit amet',
-        completed: true,
-    },
-    {
-        sectionId: 2,
-        orgAssignment: 'Org 2',
-        name: 'Jane Smith',
-        method: 'Method B',
-        notes: 'Consectetur adipiscing elit',
-        completed: false,
-    },
-    // Add more dummy data entries here
-];
-
-function App() {
+const TableDesign = () => {
+    const { client } = vendiaClient();
+    const [testData, setTestData] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
-    const [filteredData, setFilteredData] = useState(DummyData);
+    const [showModal, setShowModal] = useState(false);
+    const [editRowIndex, setEditRowIndex] = useState(null);
+    const [editedData, setEditedData] = useState({});
+
+    useEffect(() => {
+        const fetchDataFromTable = async () => {
+            try {
+                const response = await client.entities.test.list();
+                setTestData(response.items);
+            } catch (error) {
+                console.error('Error fetching data from table:', error);
+            }
+        };
+
+        fetchDataFromTable();
+    }, []);
+
+    useEffect(() => {
+        const updateDataInVendia = async () => {
+            try {
+                await client.entities.test.update(editedData);
+            } catch (error) {
+                console.error('Error updating data in Vendia:', error);
+                // You can implement any error handling logic here as needed.
+            }
+        };
+
+        updateDataInVendia();
+    }, [testData]);
 
     const handleSearch = (e) => {
         const searchText = e.target.value.toLowerCase();
-        const filteredResults = DummyData.filter((item) =>
-            item.name.toLowerCase().includes(searchText)
+        const filteredResults = testData.filter((item) =>
+            Object.values(item).some(
+                (value) =>
+                    value &&
+                    typeof value === 'string' &&
+                    value.toLowerCase().includes(searchText)
+            )
         );
         setSearchTerm(searchText);
-        setFilteredData(filteredResults);
+        setTestData(filteredResults);
     };
+    const handleEdit = (index) => {
+        setShowModal(true);
+        setEditedData(testData[index]);
+    };
+    const handleSave = async () => {
+        const { _owner, _acl, ...updatedData } = editedData; // Destructure _owner and _acl from editedData
+        const updatedTestData = [...testData];
+        updatedTestData[editRowIndex] = updatedData;
+        setTestData(updatedTestData); // Update the testData state immediately
+
+        try {
+            await client.entities.test.update(updatedData);
+            const response = await client.entities.test.get(updatedData._id); // Fetch the updated data from the API
+            const updatedTestDataAfterSave = updatedTestData.map((item) =>
+                item._id === response._id ? response : item
+            );
+            setTestData(updatedTestDataAfterSave); // Update the testData state with the response data
+            setShowModal(false);
+        } catch (error) {
+            console.error('Error updating data in Vendia:', error);
+            // You can implement any error handling logic here as needed.
+        }
+    };
+
+
+    const handleDelete = async (index) => {
+        const updatedTestData = [...testData];
+        const deletedItem = updatedTestData.splice(index, 1)[0];
+        setTestData(updatedTestData);
+
+        try {
+            await client.entities.test.remove(deletedItem._id);
+        } catch (error) {
+            console.error('Error deleting data in Vendia:', error);
+            // You can implement any error handling logic here as needed.
+        }
+    };
+
 
     return (
         <div className="container mt-5">
@@ -49,35 +104,120 @@ function App() {
                         value={searchTerm}
                         onChange={handleSearch}
                     />
-                <table className="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th style={{ border: '1px solid black' }}>Section ID</th>
-                            <th style={{ border: '1px solid black' }}>Org Assignment</th>
-                            <th style={{ border: '1px solid black' }}>Name</th>
-                            <th style={{ border: '1px solid black' }}>Method</th>
-                            <th style={{ border: '1px solid black' }}>Notes</th>
-                            <th style={{ border: '1px solid black' }}>Completed</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filteredData.map((item) => (
-                            <tr key={item.sectionId}>
-                                <td style={{ border: '1px solid black' }}>{item.sectionId}</td>
-                                <td style={{ border: '1px solid black' }}>{item.orgAssignment}</td>
-                                <td style={{ border: '1px solid black' }}>{item.name}</td>
-                                <td style={{ border: '1px solid black' }}>{item.method}</td>
-                                <td style={{ border: '1px solid black' }}>{item.notes}</td>
-                                <td style={{ border: '1px solid black' }}>{item.completed ? 'Yes' : 'No'}</td>
+                    <Table responsive striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Org Assignment</th>
+                                <th>Name</th>
+                                <th>Method</th>
+                                <th>Notes</th>
+                                <th>Completed</th>
+                                <th>Updated By</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {testData.map((row, index) => (
+                                <tr key={index}>
+                                    <td>{row.TestID}</td>
+                                    <td>{row.OrgAssignment}</td>
+                                    <td>{row.TestName}</td>
+                                    <td>{row.TestMethod}</td>
+                                    <td>{row.Notes}</td>
+                                    <td>{row.Completed ? 'Completed' : 'Not Completed'}</td>
+                                    <td>{row.UpdatedBy}</td>
+                                    <td>
+                                        <Button variant="primary" onClick={() => handleEdit(index)}>
+                                            Edit
+                                        </Button>
+                                    </td>
+                                    <td>
+                                        <Button variant="danger" onClick={() => handleDelete(index)}>
+                                            Delete
+                                        </Button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
             </div>
-        </div>
-    </div>
-        
-    );
-}
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Data</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="form-group">
+                        <label>ID</label>
+                        <input
+                            className="form-control"
+                            value={editedData.TestID}
+                            onChange={(e) => setEditedData({ ...editedData, TestID: e.target.value })}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Org Assignment</label>
+                        <input
+                            className="form-control"
+                            value={editedData.OrgAssignment}
+                            onChange={(e) => setEditedData({ ...editedData, OrgAssignment: e.target.value })}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input
+                            className="form-control"
+                            value={editedData.TestName}
+                            onChange={(e) => setEditedData({ ...editedData, TestName: e.target.value })}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Method</label>
+                        <input
+                            className="form-control"
+                            value={editedData.TestMethod}
+                            onChange={(e) => setEditedData({ ...editedData, TestMethod: e.target.value })}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Notes</label>
+                        <input
+                            className="form-control"
+                            value={editedData.Notes}
+                            onChange={(e) => setEditedData({ ...editedData, Notes: e.target.value })}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Completed</label>
+                        <input
+                            type="checkbox"
+                            checked={editedData.Completed}
+                            onChange={(e) => setEditedData({ ...editedData, Completed: e.target.checked })}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Updated By</label>
+                        <input
+                            className="form-control"
+                            value={editedData.UpdatedBy}
+                            onChange={(e) => setEditedData({ ...editedData, UpdatedBy: e.target.value })}
+                        />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowModal(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleSave}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
 
-export default App;
+        </div>
+    );
+};
+
+export default TableDesign;
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,36 +1,91 @@
-import '../App.css'
-import HeroSection from '../HeroSection'
-import '../Home.css';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { vendiaClient } from '../vendiaClient';
+import '../Home.css'; // Import the CSS file for styling
+
+const Home = () => {
+  const { client } = vendiaClient();
+  const [devices, setDevices] = useState([]);
+  const [deviceName, setDeviceName] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Add a new device to Vendia entities
+      const response = await client.entities.devices.create({
+        name: deviceName,
+        completionPercentage: 0, // Assuming completion percentage starts at 0
+      });
+
+      // Update the state with the newly created device
+      setDevices([...devices, response]);
+      setDeviceName('');
+    } catch (error) {
+      console.error('Error adding device to Vendia:', error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchDevicesFromTable = async () => {
+      try {
+        console.log('Fetching devices from table...');
+        console.log('client.entities:', client.entities); // Add this line for debugging
+
+        if (client.entities && client.entities.devices) {
+          const response = await client.entities.devices.list();
+          console.log('Response from list:', response); // Add this line for debugging
+          setDevices(response.items);
+        } else {
+          console.error('Client or client.entities.devices is undefined.');
+        }
+      } catch (error) {
+        console.error('Error fetching data from table:', error);
+      }
+    };
 
 
-function Home () {
+    fetchDevicesFromTable();
+  }, []);
+
+  const handleRemove = async (deviceId) => {
+    try {
+      // Remove the device from Vendia entities
+      await client.entities.devices.remove(deviceId);
+
+      // Update the state by removing the deleted device
+      setDevices(devices.filter((device) => device.id !== deviceId));
+    } catch (error) {
+      console.error('Error removing device from Vendia:', error);
+    }
+  };
+
   return (
-    <>
-    <HeroSection/>
-    
-    </>
-  )
-}
+    <div className="home-page">
+      <h2>Current Devices</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={deviceName}
+          onChange={(e) => setDeviceName(e.target.value)}
+        />
+        <button type="submit">Add Device</button>
+      </form>
+      <div className="device-container">
+        {devices.map((device) => (
+          <div key={device.id} className="device-item">
+            <h3>{device.name}</h3>
+            <p>Completion Percentage: {device.completionPercentage}%</p>
+            <button onClick={() => handleRemove(device.id)}>Remove</button>
+            <br />
+            <button>
+              <Link to={`/devicelist/${device.id}`}>View Tests</Link>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
-/*export const Home = () => {
-  return(
-  
-      <div className="text-center">
-          <h2>Home/Landing page </h2>
-          <p>Random text </p>
-      </div>
-  )
-}
-*/
